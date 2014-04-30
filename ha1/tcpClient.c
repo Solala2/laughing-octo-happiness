a678a94c71fdc7f04bd43c596bb268ad6064e803
Ole <itstheimp@gmail.com>
2014-04-30 14:52:03 +0200

    ha1: aufgabe 4

---
+++ ha1/tcpClient.c
@@ -0,0 +1,113 @@
+/*
+############################################################################
+#                                                                          #
+# Copyright TU-Berlin, 2011-2014                                           #
+# Die Weitergabe, Veröffentlichung etc. auch in Teilen ist nicht gestattet #
+#                                                                          #
+############################################################################
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define MAX_BUFFER_LENGTH 100
+
+int main(int argc, char *argv[])
+{
+    int sockfd;
+    struct sockaddr_in their_addr; // connector's address information
+    struct hostent *he;
+    int numbytes;
+    int serverPort;
+    int a = 0;
+    int b = 0;
+
+    printf("TCP client example\n\n");
+    
+    if (argc != 5) {
+        fprintf(stderr,"Usage: tcpClient serverName serverPort int1 int2\n");
+        exit(1);
+    }
+    
+    serverPort = atoi(argv[2]);
+    a = atoi(argv[3]);
+    b = atoi(argv[4]);    
+
+    //Resolv hostname to IP Address
+    if ((he=gethostbyname(argv[1])) == NULL) {  // get the host info
+        herror("gethostbyname");
+        exit(1);
+    }
+
+    /* ******************************************************************
+    TO BE DONE: Create socket
+    ******************************************************************* */
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (sockfd < 0) {
+		fprintf(stderr, "Failed to create socket: %i\n", sockfd);
+		return 1;
+	}
+
+    //setup transport address
+    their_addr.sin_family = AF_INET;     
+    their_addr.sin_port = htons(serverPort);
+    their_addr.sin_addr = *((struct in_addr *)he->h_addr);
+    memset(their_addr.sin_zero, '\0', sizeof their_addr.sin_zero);
+
+
+    /* ******************************************************************
+    TO BE DONE:  Connect
+    ******************************************************************* */
+
+	if (connect(sockfd, (struct sockaddr*) &their_addr, sizeof(their_addr)) != 0) {
+		fprintf(stderr, "Failed to connect\n");
+		return 1;
+	}
+    
+    unsigned char buffer[4];
+
+    packData(&buffer, a, b);
+
+    /* ******************************************************************
+    TO BE DONE:  Send data
+    ******************************************************************* */
+
+	if (send(sockfd, buffer, 4, 0) < 4) {
+		fprintf(stderr, "Failed to send\n");
+		return 1;
+	}
+
+    /* ******************************************************************
+    TO BE DONE:  Close socket
+    ******************************************************************* */
+
+	if (close(sockfd) != 0) {
+		fprintf(stderr, "Failed to shut socket down\n");
+		return 1;
+	}
+
+    return 0;
+}
+
+int packData(unsigned char *buffer, unsigned int a, unsigned int b) {
+    /* ******************************************************************
+    TO BE DONE:  pack data
+    ******************************************************************* */
+
+	buffer[0] = (a >> 8) & 255;
+	buffer[1] = a & 255;
+	buffer[2] = (b >> 8) & 255;
+	buffer[3] = b & 255;
+
+	return 4;
+}
